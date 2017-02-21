9006b95da7d1ce69572782703d6a5cf66b835a86
Claudia Nou Carrera <claudianou@gmail.com>
2017-02-21 15:40:50 +0100

    - Added classes EmptyStringConverter.cs and GuidConverter.cs in order to convert GUIS and Empty strings

---
+++ ISIC_FMT_MMCP_App/ISIC_FMT_MMCP_App/EmptyStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ISIC_FMT_MMCP_App
+{
+    public class EmptyStringConverter : IValueConverter
+    {
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            String str = (String)value;
+            return String.IsNullOrWhiteSpace(str) ? "<un-named device>" : str;
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            throw new NotImplementedException("EmptyStringConverter is one-way");
+        }
+    }
+}
